65df8e924ca66e32d43cc8477a724805c20f7e4c
JaKeyse <113917979+JaKeyse@users.noreply.github.com>
2023-05-14 11:18:31 +0300

    Мои Работы

---
+++ 2.js
@@ -0,0 +1,6 @@
+function Grettings() {
+    let name = prompt('Как вас зовут?');
+    msg = `Привет, ${name}!!`
+    alert(msg)};
+Grettings();
+console.log(msg);
---
+++ index.php
@@ -0,0 +1,152 @@
+<!ДОКТИП HTML>
+<html> _ _
+< голова >
+  < title > Мое резюме < / title >
+  < метакодировка  = " UTF-8" >
+  < meta  name = "viewport"  content = "width=width-width, initial-scale=1" >
+  < ссылка  rel = "таблица стилей"  href = "https://www.w3schools.com/w3css/4/w3.css" >
+  < link  rel = "preconnect"  href = "https://fonts.googleapis.com" >
+  < link  rel = "preconnect"  href = "https://fonts.gstatic.com" crossorigin>
+  < link  href = "https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap"  rel = "таблица стилей" >
+  < ссылка  rel = "таблица стилей"  href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" >
+  < стиль >
+    HTML,
+    тело,
+    ч1,
+    ч2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      семейство шрифтов: «Jost», без засечек;
+    }
+  < / стиль >
+< / голова >
+< класс кузова  = "w3-светло-серый" >
+  <!-- Контейнер страницы -->
+  < div  class = "w3-content w3-margin-top"  style = "max-width:1400px;" >
+    <!-- Сетка -->
+    < класс div  = "w3-row-padding" >
+      <!-- Левая колонка -->
+      < класс div  = "w3-третий" >
+        < div  class = "w3-белый w3-текст-серый w3-card-4" >
+          < div  class = "w3-display-container" >
+            < img  src = "https://imgfon.ru/Images/Details_img_880px/Animals/glaza-belyy_fon-kotik-morda-polosatyy.webp"
+              стиль = "ширина: 100%"  alt = "Аватар" >
+            < div  class = "w3-display-bottomleft w3-container w3-text-black" >
+              < h2 > Имя < / h2 >
+            < / раздел >
+          < / раздел >
+          < класс div  = "w3-контейнер" >
+            < p >< i  class = "fa fa-portre fa-fw w3-margin-right w3-large w3-text-teal" >< / i > Профессия < / p >
+            < p >< i  class = "fa fa-home fa-fw w3-margin-right w3-large w3-text-teal" >< / i > Город, страна < / p >
+            < p >< i  class = "fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal" >< / i > Электронная почта < / p >
+            < p >< i  class = "fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal" >< / i > Телефон < / p >
+ 
+            < час >
+ 
+            < p  class = "w3-large" >< b >< i  class = "fa fa-asterisk fa-fw w3-margin-right w3-text-teal" >< / i > Навыки < / b >< / p >
+            <p> Адоб Фотошоп </p> _ _ _ _
+            < div  class = "w3-светло-серый w3-круглый-xlarge w3-маленький" >
+              < div  class = "w3-container w3-center w3-round-xlarge w3-teal"  style = "width: 90%" > 90% < / div >
+            < / раздел >
+            <p> Фотография </p> _ _ _ _ _
+            < div  class = "w3-светло-серый w3-круглый-xlarge w3-маленький" >
+              < div  class = "w3-container w3-center w3-round-xlarge w3-teal"  style = "width: 80%" >
+                < div  class = "w3-center w3-text- white " > 80 % </div>
+              < / раздел >
+            < / раздел >
+            <p> Иллюстратор </p> _ _ _ _ _
+            < div  class = "w3-светло-серый w3-круглый-xlarge w3-маленький" >
+              < div  class = "w3-container w3-center w3-round-xlarge w3-teal"  style = "width: 75%" > 75% < / div >
+            < / раздел >
+            <p> Медиа </p> _ _ _ _ _
+            < div  class = "w3-светло-серый w3-круглый-xlarge w3-маленький" >
+              < div  class = "w3-container w3-center w3-round-xlarge w3-teal"  style = "width: 50%" > 50% < / div >
+            < / раздел >
+            < бр >
+ 
+            < p  class = "w3-large w3-text-theme" >< b >< i  class = "fa fa-globe fa-fw w3-margin-right w3-text-teal" >< / i > Языки < / b >
+            < / р >
+            <p> Английский </p> _ _ _ _ _
+            < div  class = "w3-светло-серый w3-круглый-xlarge" >
+              < div  class = "w3-round-xlarge w3-teal"  style = "height: 24px; width: 100%" >< / div >
+            < / раздел >
+            <p> Испанский </p> _ _ _ _ _
+            < div  class = "w3-светло-серый w3-круглый-xlarge" >
+              < div  class = "w3-round-xlarge w3-teal"  style = "height: 24px; width: 55%" >< / div >
+            < / раздел >
+            < p > Немецкий < / p >
+            < div  class = "w3-светло-серый w3-круглый-xlarge" >
+              < div  class = "w3-round-xlarge w3-teal"  style = "height: 24px; width: 25%" >< / div >
+            < / раздел >
+            < бр >
+          < / раздел >
+        < / раздел >
+        < бр >
+        <!-- Конец левой колонки -->
+      < / раздел >
+      <!-- Правая колонка -->
+      < класс div  = "w3-две трети" >
+        < div  class = "w3-контейнер w3-карта w3-белый w3-поле-нижняя часть" >
+          < класс h2  = "w3-текст-серый w3-padding-16" >< i
+              class = "fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal" >< / i > Опыт работы < / h2 >
+          < класс div  = "w3-контейнер" >
+            < h5  class = "w3-opacity" >< b > Front End Developer < / b >< / h5 >
+            < h6  class = "w3-text-teal" >< i  class = "fa fa-calendar fa-fw w3-margin-right" >< / i > Январь 2015 г. - < span
+                class = "w3-tag w3-teal w3-round" > Current < / span >< / h6 >
+            <p> Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit
+              солнце ик. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste. < / р >
+            < час >
+          < / раздел >
+          < класс div  = "w3-контейнер" >
+            < h5  class = "w3-opacity" >< b > Веб-разработчик / something.com < / b >< / h5 >
+            < h6  class = "w3-text-teal" >< i  class = "fa fa-calendar fa-fw w3-margin-right" >< / i > март 2012 г. - декабрь 2014 г. < / h6 >
+            < p > Consectetur adipisicing elit. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit
+              солнце ик. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste. < / р >
+            < час >
+          < / раздел >
+          < класс div  = "w3-контейнер" >
+            < h5  class = "w3 - opacity " > <b> Графический дизайнер / designsomething.com </b> </h5>
+            < h6  class = "w3-text-teal" >< i  class = "fa fa-calendar fa-fw w3-margin-right" >< / i > июнь 2010 г. - март 2012 г. < / h6 >
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit . </p> <br> _ _ _ _
+          < / раздел >
+        < / раздел >
+ 
+        < div  class = "w3-контейнер w3-карта w3-белый" >
+          < класс h2  = "w3-текст-серый w3-padding-16" >< i
+              class = "fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal" >< / i > Образование < / h2 >
+          < класс div  = "w3-контейнер" >
+            < h5  class = " w3 - opacity " > <b> gb.ru </b> </h5> _ _
+            < h6  class = "w3-text-teal" >< i  class = "fa fa-calendar fa-fw w3-margin-right" >< / i > навсегда < / h6 >
+            <p> Веб - разработка! Все, что мне нужно знать, в одном месте < / p >
+            < час >
+          < / раздел >
+          < класс div  = "w3-контейнер" >
+            < h5  class = "w3-opacity" >< b > Лондонская школа бизнеса < / b >< / h5 >
+            < h6  class = "w3-text-teal" >< i  class = "fa fa-calendar fa-fw w3-margin-right" >< / i > 2013 - 2015 < / h6 >
+            <p> Степень магистра </p> _ _ _ _
+            < час >
+          < / раздел >
+          < класс div  = "w3-контейнер" >
+            < h5  class = "w3-opacity" >< b > Школа кодирования < / b >< / h5 >
+            < h6  class = "w3-text-teal" >< i  class = "fa fa-calendar fa-fw w3-margin-right" >< / i > 2010 - 2013 < / h6 >
+            < p > Степень бакалавра < / p >< br >
+          < / раздел >
+        < / раздел >
+        <!-- Конец правой колонки -->
+      < / раздел >
+      <!-- Конец сетки -->
+    < / раздел >
+    <!-- Контейнер конечной страницы -->
+  < / раздел >
+  <!-- Нижний колонтитул -->
+  <footer class = "w3-container w3-teal w3-center w3-margin-top" >
+    < p > Найдите меня в социальных сетях. < / р >
+    < i  class = "fa fa-pinterest-p w3-hover-opacity" >< / i >
+    < i  class = "fa fa-twitter w3-hover-opacity" >< / i >
+    < i  class = "fa fa-linkedin w3-hover-opacity" >< / i >
+    <!-- Конец нижнего колонтитула -->
+  < / нижний колонтитул>
+< / тело >
+</html> _ _ _
